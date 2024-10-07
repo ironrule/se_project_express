7ab3acd3ca1b483a5ca8499403e5596cd352fcf5
Brandon King <brandonking25@gmail.com>
2024-10-06 23:51:34 -0400

    Updated mongoose code after correcting errors

--- app.js
+++ app.js
@@ -10,23 +10,11 @@ const { PORT = 3001 } = process.env;
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://localhost:27017/wtwr_db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
   })
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  });
-
-// mongoose
-// .connect("mongodb://localhost:27017/wtwr_db")
-// .then(() => {
-//   console.log("Connected to DB");
-// })
-// .catch(console.error);
+  .catch(console.error);
 
 app.use(cors());
 app.use(express.json());
