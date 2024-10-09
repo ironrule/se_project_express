285f33b52edfa8fa685161eb3ce234ecc135152d
Brandon King <brandonking25@gmail.com>
2024-10-09 01:07:30 -0400

    Removed crash tester code

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,6 @@ const NotFoundError = require("../errors/not-found-err");
 const { validateLogin, validateUser } = require("../middlewares/validation");
 
 router.use("/users", auth, userRouter);
-// ============================================
-//               Crash Test
-// ============================================
-const app = express();
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
-// ============================================
 router.use("/signup", validateUser, createUser);
 router.use("/signin", validateLogin, login);
 router.use("/items", clothingItemRouter);
